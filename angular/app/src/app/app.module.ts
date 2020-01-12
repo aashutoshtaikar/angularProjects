fcc326e69944f2a54c71e1012aa9a7c2e39e9b4e
Taikar <aashutoshtaikar@gmail.com>
2020-01-11 20:41:09 -0500

    added app

---
+++ angular/app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { SummaryPipe } from './summary.file';
+// import { CoursesService } from './services/courses/courses.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    SummaryPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    // CoursesService //this is not required from angular6 can be included in service decorator as providedIn
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/app/src/app/courses/courses.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../services/courses/courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+  title = "List of courses";
+  courses;
+  imageUrl = "https://www.gstatic.com/webp/gallery3/1.sm.png"
+  colSpan = 2;
+  isActive = true;
+
+  course = {
+    title: "The Complete Angular Course",
+    rating: 4.9745,
+    students: 30123,
+    price: 190.25,
+    releaseDate: new Date(2016, 3, 1)
+  }
+
+  //two way data binding
+  someObject;
+
+  constructor(coursesService: CoursesService) { 
+    this.courses = coursesService.getCourses();
+  }
+
+  ngOnInit() {
+  }
+
+  //EVENT BINDING
+  onSave($event){
+    // $event.stopPropagation(); ////prevent event bubbling
+    console.log(event);
+    console.log(event.srcElement)
+    console.log('button was clicked');
+  }
+
+  onDivClicked(){
+    console.log('div was clicked');
+  }
+
+  onKeyUp($event){
+    console.log( $event.keyCode + 'key was pressed');
+    if($event.keyCode === 13) console.log("Enter key pressed");
+  }
+
+  onEnterKeyUp(){
+    console.log("Enter key pressed");
+  }
+
+  onKeyEvVar($event){
+    console.log($event.target.value);
+  }
+
+  onKeyVar(email){
+    console.log(email);
+    console.log(email.value);
+  }
+
+  onKeyUpdateSomeObject(){
+    console.log(this.someObject);
+  }
+}
